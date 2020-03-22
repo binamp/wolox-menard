6b2e4c982ee288e285c9289f96c4860eb0453bb8
JoniMujica <jonimujica3@hotmail.com>
2020-03-22 17:02:06 -0300

    List of objects by items , added

--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,43 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import navstyle from './navstyle.module.scss';
 import Logo from '../../image/toolbar/logo.jpeg'
 
 class Navbar extends React.Component {
 
     render() {
+        const items = [
+            { title: "Polo lessons" , url: "/lessons" },
+            { title: "Polo Holidays" , url: "/holidays" },
+            { title: "Livery Services" , url: "/services" },
+            { title: "Team Management" , url: "/management" },
+            { title: "Horses for Sale" , url: "/sale" },
+            { title: "Contact US" , url: "/contact" }
+        ]
         return (
             <header className={navstyle.toolBar}>
 
                 <nav className={navstyle.toolBarNav}>
                     <div className={navstyle.toolBarLogo}>
 
-                        <Link to="/" >
+                        <NavLink to="/" >
                             <img src={Logo} alt="Home" />
-                        </Link>
+                        </NavLink>
                     </div>
                     <div className={navstyle.spacer} />
                     <div className={navstyle.toolBarItems}>
                         <ul>
-                            <li>
-                                <Link to="/lessons" className={navstyle.toolBarRef} >Polo lessons</Link>
-                            </li>
-                            <li>
-                                <Link to="/holidays" className={navstyle.toolBarRef} >Polo Holidays </Link>
-                            </li>
-                            <li>
-                                <Link to="/services" className={navstyle.toolBarRef} >Livery Services</Link>
-                            </li>
-                            <li>
-                              <Link to="/management" className={navstyle.toolBarRef}  >Team Management</Link>
-                            </li>
-                            <li>
-                                <Link to="/sale" className={navstyle.toolBarRef} >Horses for Sale</Link>
-                            </li>
-                            <li>
-                                <Link to="/contact" className={navstyle.toolBarRef} >Contact US</Link>
-                            </li>
+                            {
+                                items.map((n,index)=>
+                                    <li key={index}>
+                                        <NavLink activeClassName={navstyle.toolBarRefActive} className={navstyle.toolBarRef} to={n.url}>{n.title}</NavLink>
+                                    </li>
+                                )
+                            }
                         </ul>
+                        {/* <ul>
+                            <li>
+                                <NavLink activeClassName={navstyle.toolBarRefActive} className={navstyle.toolBarRef} to="/lessons" >Polo lessons</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/holidays" className={navstyle.toolBarRef} >Polo Holidays </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/services" className={navstyle.toolBarRef} >Livery Services</NavLink>
+                            </li>
+                            <li>
+                              <NavLink to="/management" className={navstyle.toolBarRef}  >Team Management</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/sale" className={navstyle.toolBarRef} >Horses for Sale</NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contact" className={navstyle.toolBarRef} >Contact US</NavLink>
+                            </li>
+                        </ul> */}
                     </div>
                 </nav>
             </header>
