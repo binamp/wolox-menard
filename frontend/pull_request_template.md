cd69880c3a4f34d60d26ee0d9f1d4358fea220f9
JoniMujica <jonimujica3@hotmail.com>
2020-03-24 17:18:18 -0300

    I created the Team Management component structure

---
+++ frontend/pull_request_template.md
@@ -0,0 +1,22 @@
+## Summary
+Cree una barra de herramientas con direcciones a otras secciones de la pagina (navbar).
+
+
+Problemas:
+tuve problemas al intentar animar el cambio de estilo del nav en cada seccion.
+
+Mejoras:
+
+-Mejore el estilo de uso, en vez de usar Link en cada boton, se cambio a NavLink
+
+-Se redujo codigo de seccion del menu, en vez de usar la etiqueta 'NavLink' en cada item, se imprime el menu mediante un array (lista) de objetos que contiene los datos que hara cada 'NavLink' y es recorrida por un 'map', sirve para ser re utilizado en caso de agregar mas secciones
+
+Consideracion:
+
+-agregarle animacion (translate) al estilo del NavLink al momento de cambiar de seccion
+
+-Arreglar/cambiar el Logo del Menu
+
+## Screenshots
+
+![](img.png)
--- frontend/src/components/pages/TeamManagement/teammanagement.js
+++ frontend/src/components/pages/TeamManagement/teammanagement.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import Navbar from '../../navbar/navbar.js';
 
-function TeamManagement () {
-    return(
-        <>
-            <Navbar />
-            <h1>Welcome to TeamManagement</h1>
-        </>
-    )
+import Navbar from '../../navbar/navbar';
+
+import tmanagement from './tmanagement.module.scss';
+
+const Cuadrilla = () => (
+  <>
+    <div className={tmanagement.cuadroContact}>
+      <div className={tmanagement.centrar}>
+        <h1>Team Management</h1>
+        <hr />
+        <span className={tmanagement.subtitle}>MAKING THE NEXT CHAMPIONS</span>
+        <p className={tmanagement.text}>
+          holaaaaaaaaaaaaaaaassssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss
+        </p>
+      </div>
+    </div>
+    <div className={tmanagement.cuadroGallery}>
+      <div className={tmanagement.Gallery} />
+    </div>
+  </>
+);
+
+// eslint-disable-next-line react/no-multi-comp
+// const Cuadro = () => (
+//   <div className={tmanagement.cuadro}>
+//     <Cuadrilla />
+//   </div>
+// );
+
+// eslint-disable-next-line react/no-multi-comp
+// const Cuad = () => (
+//   <div className={tmanagement.cuadimg}>
+//     <Cuadro />
+//   </div>
+// );
+
+// eslint-disable-next-line react/no-multi-comp
+function TeamManagement() {
+  return (
+    <div className={tmanagement.comp}>
+      <Navbar />
+      {/* <Cuad /> */}
+      <div className={tmanagement.cuadimg}>
+        <div className={tmanagement.cuadro}>
+          <Cuadrilla />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TeamManagement;
