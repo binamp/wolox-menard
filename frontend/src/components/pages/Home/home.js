426e22812ead8b3b52db2c5f48adb485978372d7
agusahumada <agustinaahumada649@gmail.com>
2020-03-29 02:37:13 -0300

    new color changes in Our services section

--- frontend/src/components/pages/Home/home.js
+++ frontend/src/components/pages/Home/home.js
@@ -1,16 +1,14 @@
-import React from 'react'
-import Welcome from  './welcome'
-import OurServices from './ourServices'
-import styles from './styles.module.scss'
+import React from "react";
+import Welcome from "./welcome";
+import OurServices from "./ourServices";
+import styles from "./styles.module.scss";
 
-
-function Home (){
-    return(
-        <div className={styles.background}>
-            <Welcome />                               
-            <OurServices />
-        </div>
-    )
+function Home() {
+  return (
+    <div className={styles.background}>
+      <Welcome />
+      <OurServices />
+    </div>
+  );
 }
 export default Home;
-
